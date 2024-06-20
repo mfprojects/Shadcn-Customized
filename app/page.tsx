8dee8e43dbb96d62ba735c9626964fbff5dcf759
Mikkel <fjeldmikkel@gmail.com>
2024-06-20 21:04:28 +0200

    Fixed som col spacing issues and learned along the way how the grid sizing works.

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,25 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="p-24">
+    <main className="p-12">
       <Nav />
-      <div className="grid grid-row-auto grid-cols-7 gap-4 p-4">
-        <section className="col-start-3 col-end-6 py-12 flex flex-col items-center text-center gap-8">
-          <h1 className="text-5xl font-medium leading-12">
+      <div className="flex flex-col pt-16">
+        <section className="pt-0 xs:pt-0 pb-12 flex flex-col items-center text-center gap-8 relative">
+          <h1 className="text-5xl font-medium leading:10 lg:leading-12">
             Shadcn is what we use
           </h1>
           <p className="text-2xl leading-7 text-muted-foreground">
             Customize and create
           </p>
         </section>
-        <div className="col-start-1 col-span-7 sm:col-start-2 md:col-start-3 sm:col-span-5 md:col-span-3 pb-6 flex-1 flex-col items-center gap-8">
-          <UserForm />
-        </div>
-        <div className="col-start-4 flex gap-6 items-center justify-center">
-          <Button variant={'secondary'}>Learn More</Button>
-          <Button>Enroll Now</Button>
+        <div className="grid grid-row grid-cols-3 gap-4 p-4">
+          <div className="col-start-1 col-span-3 lg:col-start-2 lg:col-span-1 pb-6 flex-col items-center gap-8">
+            <UserForm />
+          </div>
+          <div className="col-span-1 col-start-2 flex gap-6 items-center justify-center">
+            <Button variant={'secondary'}>Learn More</Button>
+            <Button>Enroll Now</Button>
+          </div>
         </div>
       </div>
     </main>
